471aa4f67c0183b36ec418388f3bfec83c5d18b6
Robert Basic <robertbasic.com@gmail.com>
2010-04-24 13:39:15 +0200

    just to create the PPN lib folder

---
+++ library/PPN/Model/Abstract.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Abstract
+ *
+ * @author robert
+ */
+class PPN_Model_Abstract
+{
+}
