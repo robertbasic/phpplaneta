cc89a300247e19786fd54ffb66fa59c78ac55d0a
Robert Basic <robertbasic.com@gmail.com>
2010-04-24 18:24:58 +0200

    methods in PPN_Model_Abstract for getting resources/forms

--- application/modules/public/models/News.php
+++ application/modules/public/models/News.php
@@ -1,11 +1,7 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 /**
- * Description of News
+ * News model
  *
  * @author robert
  */
