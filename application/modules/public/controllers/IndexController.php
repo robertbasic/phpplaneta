a5d32fd35f6942f8ef8d23cf23dff24cdcbc46c8
Robert Basic <robertbasic.com@gmail.com>
2010-04-24 13:56:34 +0200

    just to see if the autoloader finds everything...

--- application/modules/public/controllers/IndexController.php
+++ application/modules/public/controllers/IndexController.php
@@ -5,6 +5,7 @@ class IndexController extends Zend_Controller_Action
 
     public function init()
     {
+        $this->model = new Public_Model_News();
     }
 
     public function indexAction()
---
+++ application/modules/public/models/News.php
@@ -0,0 +1,14 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of News
+ *
+ * @author robert
+ */
+class Public_Model_News extends PPN_Model_Abstract
+{
+}
