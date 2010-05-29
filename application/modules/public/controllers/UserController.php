90c253674534b385ea36bd06fbc6e232049b738c
Robert Basic <robertbasic.com@gmail.com>
2010-05-29 18:33:13 +0200

    a separate layout for the login form, plus some notifications during login/logut

--- application/modules/public/controllers/UserController.php
+++ application/modules/public/controllers/UserController.php
@@ -6,6 +6,8 @@ class UserController extends Zend_Controller_Action
     public function init()
     {
         $this->auth = new Planet_Service_Auth();
+        
+        $this->fm = $this->getHelper('flashMessenger');
     }
 
     public function indexAction()
@@ -14,17 +16,18 @@ class UserController extends Zend_Controller_Action
 
     public function loginAction()
     {
+        $this->_helper->layout->setLayout('login');
+        
         $loginForm = new Planet_Form_User_Login();
         
         if($this->_request->isPost()) {
             if($loginForm->isValid($this->_request->getPost())) {
                 $data = $loginForm->getValues();
                 if($this->auth->authenticate($data['email'], $data['password'])) {
+                    $this->fm->addMessage(array('fm-good' => 'Uspešno ste se prijavili!'));
                     return $this->_helper->redirector->gotoUrl('/admin');
                 } else {
-                    /**
-                     * @todo add failed authentication message to flashmessenger
-                     */
+                    $this->fm->addMessage(array('fm-bad' => 'Pogrešno korisniško ime i/ili lozinka!'));
                 }
             }
         }
@@ -35,9 +38,9 @@ class UserController extends Zend_Controller_Action
     public function logoutAction()
     {
         $this->auth->clear();
-        /**
-         * @todo add successful logout message to flashmessenger
-         */
+
+        $this->fm->addMessage(array('fm-good' => 'Uspešno ste se odjavili!'));
+
         return $this->_helper->redirector->gotoUrl('/');
     }
 
