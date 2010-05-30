c30d79c1f32b040bf632562c1045e5559d4948e2
Robert Basic <robertbasic.com@gmail.com>
2010-05-30 13:02:12 +0200

    added logging. writes to an xml file if logging is enabled - enabled on production, disabled on dev

--- application/modules/public/controllers/ErrorController.php
+++ application/modules/public/controllers/ErrorController.php
@@ -16,12 +16,11 @@ class ErrorController extends Zend_Controller_Action
 
         switch (get_class($errors->exception)) {
             case 'Zend_Controller_Dispatcher_Exception':
-                // send 404
-                $this->getResponse()
-                     ->setRawHeader('HTTP/1.1 404 Not Found');
-                $this->view->message = 'Tražena stranica nije pronađena.';
+                $this->_setParam('error_handler', $errors);
+                return $this->_forward('not-found');
                 break;
             default:
+                $this->_logError($errors, 2);
                 // application error
                 $stackTrace = $errors->exception->getTraceAsString();
                 $message = $errors->exception->getMessage();
@@ -34,12 +33,27 @@ class ErrorController extends Zend_Controller_Action
     }
 
     public function notFoundAction()
-    {
+    {        
         $errors = $this->_getParam('error_handler');
+
+        $this->_logError($errors);
 
         $this->getResponse()
              ->setRawHeader('HTTP/1.1 404 Not Found');
         $this->view->message = $errors->exception->getMessage();
+    }
+
+    protected function _logError($errors, $level=3)
+    {
+        try {
+            $logger = Zend_Registry::get('logger');
+            $logger->log(
+                    $errors->exception->getMessage() . "\n" .
+                    $errors->exception->getTraceAsString() . "\n" .
+                    var_export($errors->request->getParams(), true),
+                    $level);
+        } catch (Exception $e) {
+        }
     }
 
 }
