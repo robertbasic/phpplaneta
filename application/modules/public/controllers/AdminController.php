464e882a53243866f6dff622632fedd0073cab1c
Robert Basic <robertbasic.com@gmail.com>
2010-06-21 23:12:25 +0200

    starting to work on settings view/change via admin panel

--- application/modules/public/controllers/AdminController.php
+++ application/modules/public/controllers/AdminController.php
@@ -11,4 +11,13 @@ class AdminController extends Zend_Controller_Action
     {
     }
 
+    public function settingsAction()
+    {
+        $config = new Zend_Config_Ini(APPLICATION_PATH . '/configs/application.ini', APPLICATION_ENV);
+        
+        $this->view->environment = APPLICATION_ENV;
+        $this->view->cacheSetting = $config->settings->cache->enabled;
+        $this->view->logSetting = $config->settings->log->enabled;
+    }
+
 }
