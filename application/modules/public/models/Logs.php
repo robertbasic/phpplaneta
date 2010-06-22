5afedd56ccb780efdcdc81e9435e746438d2f674
Robert Basic <robertbasic.com@gmail.com>
2010-06-22 22:03:59 +0200

    viewing the log messages via the admin panel

---
+++ application/modules/public/models/Logs.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Logs model
+ *
+ * @author robert
+ */
+class Planet_Model_Logs extends PPN_Model_Abstract
+{
+    public function __construct()
+    {
+    }
+
+    public function getAllLogs($page=null)
+    {
+        return $this->getResource('Logs')->getAllLogs($page);
+    }
+}
