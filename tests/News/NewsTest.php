00c2dde52307e1a479fd66e20e7823da36eff714
Robert Basic <robertbasic.com@gmail.com>
2011-01-28 07:20:02 +0100

    Setup things for writing and running tests

---
+++ tests/News/NewsTest.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once APPLICATION_PATH . '/modules/public/models/News.php';
+
+class News_Model_NewsTest extends PHPUnit_Framework_TestCase
+{
+    public function testTestsAreRuning()
+    {
+        $i = 1;
+        $this->assertEquals(1, $i);
+    }
+}
