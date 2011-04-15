929170eb073266c6e245e73bc19d4567383000a2
Robert Basic <robertbasic.com@gmail.com>
2011-04-15 20:16:00 +0200

    making the user model test pass

--- application/modules/public/models/User.php
+++ application/modules/public/models/User.php
@@ -12,9 +12,21 @@ class Planet_Model_User extends PPN_Model_Abstract
         
     }
 
+    /**
+     *
+     * @param string $email
+     * @return Planet_Model_Resource_User_Item
+     * @throws PPN_Exception_NotFound
+     */
     public function getUserByEmail($email)
     {
-        return $this->getResource('User')->getUserByEmail($email);
+        $user = $this->getResource('User')->getUserByEmail($email);
+        
+        if($user === null) {
+            throw new PPN_Exception_NotFound();
+        }
+        
+        return $user;
     }
 
 }
